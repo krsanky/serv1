206d4ba9b1ef5c69e5edbe57b6983b144ea2d6b1
paul wisehart <paul@oldcode.org>
2018-01-11 09:11:38 -0500

    start to add log4j2

--- src/main/java/org/oldcode/javaweb/MainServlet.java
+++ src/main/java/org/oldcode/javaweb/MainServlet.java
@@ -5,16 +5,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class MainServlet extends HttpServlet {
 
+    private static final Logger log = LogManager.getLogger(MainServlet.class);
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
+        log.error("GET ...");
         response.getWriter().print("main servlet GET");
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
         response.getWriter().print("main servlet POST");
     }
 
