36413aa58efd97e6c10ed89c6b94d97fbe33ce25
paul wisehart <paul@oldcode.org>
2018-01-10 20:28:34 -0500

    basic war kinda works

---
+++ src/main/java/org/oldcode/javaweb/HelloServlet.java
@@ -0,0 +1,28 @@
+package org.oldcode.javaweb;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        String s1 = "asdasd";
+        response.getWriter().print("Hello, World!");  
+    }
+
+    /*
+    @override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name == null) name = "World";
+        request.setAttribute("user", name);
+        request.getRequestDispatcher("response.jsp").forward(request, response); 
+    }
+    */
+}
+
