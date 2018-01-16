556240e43e15f0cea29eff9dbcde909f806fedde
paul wisehart <paul@oldcode.org>
2018-01-15 20:22:20 -0500

    ...

--- src/main/java/org/oldcode/javaweb/db/Conn.java
+++ src/main/java/org/oldcode/javaweb/db/Conn.java
@@ -4,12 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 
 // A web application has to explicitly close ResultSet's, Statement's, and Connection's.
-public class Conn {
+public class Conn { //change name to some variation of database? or DbUtil ? dbstuff Database ...
 
     private static final Logger log = LogManager.getLogger(Conn.class);
 
@@ -35,36 +34,7 @@ public class Conn {
     }
 
     public void testConn() {
-        /*
-        Settings settings = MainServlet.getSettings();
-        String user = settings.get("db_user");
-        String password = settings.get("db_password");
-        String name = settings.get("db_name");
-        String url = String.format("jdbc:postgresql://localhost/%s?user=%s&password=%s", name, user, password);
-        try {
-            Connection conn = DriverManager.getConnection(url);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
-        */
-
-        // scratch the above, we use tomcat for DBCP connections
-        try {
-            InitialContext cxt = new InitialContext();
-            if (cxt == null) {
-                //throw new Exception("Uh oh -- no context!");
-                log.error("InitalContext is null ... bad");
-            }
-
-            DataSource ds = (DataSource) cxt.lookup("java:/comp/env/jdbc/postgres");
-
-            if (ds == null) {
-                //throw new Exception("Data source not found!");
-                log.error("DataSource is null ... bad");
-            }
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
+        // mv the jdbc example from MainServlet here?
     }
 
 }
