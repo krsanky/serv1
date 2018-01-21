5937f59575ecbefacb5f7fe2f25a1322223c40f7
paul wisehart <paul@oldcode.org>
2018-01-21 12:12:18 -0500

    preliminary jooq support

---
+++ bin/jooq.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+BASEDIR=$HOME/JAVA/xyz
+JOOQ_LIB=$BASEDIR/jooq-libs
+
+cd $BASEDIR
+
+
+CP="$JOOQ_LIB/jooq-3.10.4.jar"
+CP="$CP:$JOOQ_LIB/jooq-codegen-3.10.4.jar"
+CP="$CP:$JOOQ_LIB/jooq-meta-3.10.4.jar"
+CP="$CP:$JOOQ_LIB/postgresql-42.1.4.jar"
+CP="$CP:."
+
+java \
+-classpath $CP \
+org.jooq.util.GenerationTool \
+webserver.xml
+
